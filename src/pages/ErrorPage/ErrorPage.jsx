7f2c2091337e272ab3ff2edf5dee27eb1ea176eb
Shamim-DM <mdshamimmiah000000@gmail.com>
2023-05-05 23:04:02 +0600

    deploy all and complete

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -4,21 +4,21 @@ import img from '../../assets/404-img.jpg' ;
 const ErrorPage = () => {
     const { error, status } = useRouteError()
     return (
-        <section className='flex items-center h-screen p-16 bg-purple-100 text-purple-900'>
+        <section className='flex items-center h-screen p-16  text-purple-900 mt-12'>
       <div className='container flex flex-col items-center justify-center px-5 mx-auto my-8'>
         <div className='rounded-lg'>
-          <img className='rounded-full' src={img} alt="" />
+          <img className='rounded-full w-[60%] mx-auto' src='https://img.freepik.com/free-vector/oops-404-error-with-broken-robot-concept-illustration_114360-5529.jpg?w=2000' alt="" />
         </div>
         <div className='max-w-md text-center'>
           <h2 className='mb-8 font-extrabold text-9xl text-purple-600'>
             <span className='sr-only'>Error </span> 
           </h2>
           <p className='text-2xl font-semibold md:text-3xl mb-8'>
-            you are at wrong place!!!
+            You are at wrong place!!!
           </p>
           <Link
             to='/'
-            className='px-8 py-3 font-semibold rounded bg-purple-500 text-purple-900'
+            className='px-8 py-3 font-semibold rounded bg-purple-500 text-white mb-4'
           >
             Back to Home
           </Link>
